bab2a2e87a2f4c5fb18449f7d91107393aee2bfb
Hauke D <haukex@zero-g.net>
2021-03-12 15:15:01 +0100

    Refactored math.pl internals to use Autoactions

--- math.pl
+++ math.pl
@@ -5,17 +5,32 @@ use IO::Interactive 'is_interactive';
 
 my $input = join ' ', @ARGV;
 
+package Actions {
+	sub new { bless {}, shift }
+	sub add {
+		my ($self, $match) = @_;
+		my ($term, $op) = @$match{qw/term op/};
+		my $out = shift $term->@*;
+		return $out if !$term->@* && !ref $op;
+		die unless $term->@* == $op->@*;
+		for my $i ( 0 .. $term->$#* ) {
+			if ( $$op[$i] eq '+' ) { $out += $$term[$i] }
+			else { die }
+		}
+		return $out;
+	}
+}
+
 my $grammar = do { use Regexp::Grammars; qr{
 	<nocontext:>
 	\A <result=expr> \z
 	<rule: expr>
 		(?: <MATCH=number> | <MATCH=add> ) <.ws>
 	<rule: add>
-		<[number]>+ % <[op=(\+)]>
-		(?{ $MATCH=0; $MATCH+=$_ for $MATCH{number}->@* })
+		<[term=number]>+ % <[op=(\+)]>
 	<token: number>
 		[-+]?\d+
-}xms };
+}xms }->with_actions(Actions->new);
 
 $input =~ $grammar or die "Failed to parse input.\n";
 print $/{result};
